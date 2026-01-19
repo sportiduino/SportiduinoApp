c4f55aef7b88e8494366efe4e2603ab806568331
myadzel <myadzel@gmail.com>
2026-01-19 21:38:07 +0300

    Allow to collect (and skip in scoring) start and finish points for rogaining

--- app/src/main/java/org/sportiduino/app/CourseValidatorRogaining.java
+++ app/src/main/java/org/sportiduino/app/CourseValidatorRogaining.java
@@ -1,10 +1,28 @@
 package org.sportiduino.app;
 
+import static org.sportiduino.app.sportiduino.Config.FINISH_STATION;
+import static org.sportiduino.app.sportiduino.Config.START_STATION;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class CourseValidatorRogaining extends CourseValidatorBase {
     public CourseValidatorRogaining(CourseValidator validator) {
         super(validator);
+    }
+
+    public ArrayList<Integer> normalizeCardPoints(ArrayList<Integer> points) {
+        ArrayList<Integer> normalizedPoints = new ArrayList<>();
+
+        for (Integer point : points) {
+            if (Objects.equals(point, START_STATION) || Objects.equals(point, FINISH_STATION)) {
+                continue;
+            }
+
+            normalizedPoints.add(point);
+        }
+
+        return normalizedPoints;
     }
 
     public Boolean isValid() {
