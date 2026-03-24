ed4e29db011a3e7f74da3f45ce4f2f09242ae2f9
myadzel <myadzel@gmail.com>
2026-03-24 21:58:08 +0300

    Add start and finish points to validation for rogaining

--- app/src/main/java/org/sportiduino/app/CourseValidatorRogaining.java
+++ app/src/main/java/org/sportiduino/app/CourseValidatorRogaining.java
@@ -3,6 +3,7 @@ package org.sportiduino.app;
 import org.sportiduino.app.sportiduino.Config;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CourseValidatorRogaining extends CourseValidatorBase {
     public CourseValidatorRogaining(CourseValidator validator) {
@@ -10,7 +11,39 @@ public class CourseValidatorRogaining extends CourseValidatorBase {
     }
 
     public Boolean isValid() {
+        boolean hasStartPoint = false;
+        boolean hasFinishPoint = false;
+
+        ArrayList<Integer> cardPoints = validator.getCardPoints();
+
+        // check start and finish first
+        for (int cardPoint : cardPoints) {
+            if (cardPoint == Config.START_STATION) {
+                hasStartPoint = true;
+                continue;
+            }
+
+            if (cardPoint == Config.FINISH_STATION) {
+                hasFinishPoint = true;
+            }
+        }
+
+        if (!hasStartPoint || !hasFinishPoint) {
+            return false;
+        }
+
         return !validator.getCardPoints().isEmpty();
+    }
+
+    public ArrayList<Integer> getMissedCoursePoints() {
+        ArrayList<Integer> coursePoints =
+                new ArrayList<>(List.of(Config.START_STATION, Config.FINISH_STATION));
+        ArrayList<Integer> cardPoints = validator.getCardPoints();
+
+        ArrayList<Integer> missedCoursePoints = new ArrayList<>(coursePoints);
+        missedCoursePoints.removeAll(cardPoints);
+
+        return missedCoursePoints;
     }
 
     public Integer getScore() {
